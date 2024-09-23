c030d92ba70cad916c329f22036cb67c74be7629
mariawsx <claramaria91881@gmail.com>
2024-09-23 13:29:27 -0300

    Update casinha.py

--- casinha.py
+++ casinha.py
@@ -1,16 +1,15 @@
 def desenhar_casinha(tamanho):
-    # Verifica se o tamanho é suficiente para desenhar uma casinha
+ 
     if tamanho < 3:
         print("O tamanho deve ser maior ou igual a 3.")
         return
 
-    # Desenhar o telhado
+  
     for i in range(tamanho):
         espacos = ' ' * (tamanho - i - 1)
         barras = '\\' * (4 * i + 1)
         print(espacos + barras + espacos)
 
-    # Desenhar as paredes e o teto
     largura = 4 * tamanho - 1
     for i in range(tamanho):
         if i == tamanho - 1:
@@ -18,9 +17,9 @@ def desenhar_casinha(tamanho):
         else:
             print('|' + ' ' * (largura - 2) + '|')
 
-    # Desenhar o chão
+
     print('|' + '_' * (largura - 1) + '|')
 
-# Exemplo de uso
+
 tamanho = 10
 desenhar_casinha(tamanho)
